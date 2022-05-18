05b6302f62db21fb67def11d056fc30a8da10468
JQUAN CALEK <jquancollins96@gmail.com>
2022-05-18 12:04:50 -0400

    New Fruits

--- server.js
+++ server.js
@@ -10,21 +10,44 @@ const port = 3000;
 
 
 
-
-
+//mount middleware
+// app.use((req,res,next) =>{
+//   console.log(req.body)
+//   next()
+// })
+// this adds data to req.body so we can access it in the create action
+app.use(express.urlencoded({ extended: false }))
+//mount routes
 
 // INDEX
 app.get('/fruits/', (req, res) => {
  res.render('index.ejs', {allFruits: fruits});
 });
 
-// N
+// New
 
+app.get('/fruits/new',(req,res) =>{
+  res.render('new.ejs')
+})
 // D
 
 // U
 
 // C
+app.post("/fruits", (req, res) => {
+  if (req.body.readyToEat === "on") {
+    //if checked, req.body.readyToEat is set to 'on'
+    req.body.readyToEat = true //do some data correction
+  } else {
+    //if not checked, req.body.readyToEat is undefined
+    req.body.readyToEat = false //do some data correction
+  }
+  fruits.push(req.body)
+  console.log(fruits)
+  res.redirect("/fruits")
+  res.send("data received")
+})
+
 
 // E
 
